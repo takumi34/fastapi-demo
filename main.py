95ef18ea6cfbcc174e5f7bad84c8e51a902f0d14
takumi34 <1shiranui@gmail.com>
2020-02-03 23:28:16 +0900

    update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/chika")
+def read_chika():
+    return {"Chika"}
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
